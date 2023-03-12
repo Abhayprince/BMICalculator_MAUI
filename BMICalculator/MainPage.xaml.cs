086ba62f5a7fdc3af0a2638e5343ce321f375140
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2023-03-13 01:06:54 +0530

    Part-2 Calculate BMI image button, Bottom info lables. Calculate BMI Logic

--- BMICalculator/MainPage.xaml.cs
+++ BMICalculator/MainPage.xaml.cs
@@ -1,11 +1,19 @@
-﻿namespace BMICalculator;
+﻿using System.ComponentModel;
+
+namespace BMICalculator;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
+    private void CalculateBMI_Pressed(object sender, EventArgs e)
+    {
+        var heightInMeter = heightSlider.Value * 100;
+        var weightInKg = weightSlider.Value;
+
+        var bmi = weightInKg / (heightInMeter * heightInMeter);
+    }
 }
-
