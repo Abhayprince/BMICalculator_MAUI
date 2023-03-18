bb807665c86e1b102f1d9ef7909bb2dddae467ea
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2023-03-19 01:19:10 +0530

    Part - 5 - Bmi calculate functionality, viewModel and Reset button functionality

--- BMICalculator/MainPage.xaml.cs
+++ BMICalculator/MainPage.xaml.cs
@@ -4,18 +4,100 @@ namespace BMICalculator;
 
 public partial class MainPage : ContentPage
 {
+    private BmiViewModel _viewModel;
     public MainPage()
     {
         InitializeComponent();
+        _viewModel = new BmiViewModel();
+        BindingContext = _viewModel;
     }
 
-    private async void CalculateBMI_Pressed(object sender, EventArgs e)
+    private void CalculateBMI_Pressed(object sender, EventArgs e)
     {
-        var heightInMeter = heightSlider.Value * 100;
+        var heightInMeter = heightSlider.Value / 100;
         var weightInKg = weightSlider.Value;
 
         var bmi = weightInKg / (heightInMeter * heightInMeter);
 
-        Shell.Current.DisplayAlert("BMI", $"Bmi is {bmi}", "Ok");
+        _viewModel.Bmi = bmi.ToString("N2");
+        (_viewModel.BmiStatus, _viewModel.StatusColor) = GetBmiStatus(bmi);
+        _viewModel.IsCalculated = true;
+    }
+    private static (string status, Color color) GetBmiStatus(double bmi)
+    {
+        if (bmi < 18.5)
+            return ("You are Underweight", Colors.Blue);
+        else if (bmi <= 24.9)
+            return ("You have a Normal body weight, Great Job!", Colors.Green);
+        else if (bmi <= 29.9)
+            return ("You are Overweight", Colors.Yellow);
+        else
+            return ("You are Obese", Colors.DarkRed);
+    }
+
+    private void ResetBMI_Pressed(object sender, EventArgs e)
+    {
+        heightSlider.Value = 10;
+        weightSlider.Value = 10;
+        _viewModel.Reset();
+    }
+}
+
+public class BmiViewModel : INotifyPropertyChanged
+{
+    private string _bmi;
+    public string Bmi
+    {
+        get => _bmi;
+        set
+        {
+            _bmi = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Bmi)));
+        }
+    }
+
+    private string _bmiStatus;
+    public string BmiStatus
+    {
+        get => _bmiStatus;
+        set
+        {
+            _bmiStatus = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BmiStatus)));
+        }
+    }
+    private Color _statusColor = Colors.Blue;
+    public Color StatusColor
+    {
+        get => _statusColor;
+        set
+        {
+            _statusColor = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(StatusColor)));
+        }
+    }
+
+    private bool _isCalculated;
+    public bool IsCalculated
+    {
+        get => _isCalculated;
+        set
+        {
+            _isCalculated = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsCalculated)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsNotCalculated)));
+        }
+    }
+
+    public bool IsNotCalculated => !IsCalculated;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void Reset()
+    {
+        Bmi = string.Empty;
+        BmiStatus = string.Empty;
+        StatusColor = Colors.Blue;
+        IsCalculated = false;
     }
 }
